35420788c89b61fcfaec4ecbcd0eb077972116ea
alias09inc <kenshi06182001@gmail.com>
2024-04-03 14:43:16 +0900

    did problem20

---
+++ chapter03/20.py
@@ -0,0 +1,15 @@
+import json
+
+decoder = json.JSONDecoder()
+
+with open('jawiki-country.json', 'r') as f:
+    line = f.readline()
+    while line:
+        line_json=decoder.decode(line)
+        country_name = line_json['title']
+        if country_name != 'イギリス':
+            line = f.readline()
+        else:
+            print(country_name)
+            print(line_json['text'])
+            break
