7d32c8562a5f777a4e0576c6f58f2861768de88a
alias09inc <73377814+alias09inc@users.noreply.github.com>
2024-04-20 18:35:23 +0900

    finished 30.py

--- chapter04/30.py
+++ chapter04/30.py
@@ -1,3 +1,27 @@
-with open("neko.txt.mecab", "r") as f:
-    for line in f:
-        print(line)
+def parse_mecab(block):
+    res = []
+    for line in block.split('\n'):
+        if line == '':
+            return res
+        (surface, attrs) = line.split('\t')
+        attr = attrs.split(',')
+        base = attr[6]
+        pos = attr[0]
+        pos1 = attr[1]
+        linedict = {
+            'surface': surface,
+            'base': base,
+            'pos': pos,
+            'pos1': pos1,
+        }
+        res.append(linedict)
+
+
+filename = 'neko.txt.mecab'
+mapping_lists = []
+
+with open(filename, mode='rt', encoding='utf-8') as f:
+    blocks = f.read().split('EOS\n')
+blocks = list(filter(lambda x: x!= '', blocks))
+blocks = [parse_mecab(block) for block in blocks]
+print(blocks[5])
